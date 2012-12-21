69c76e52fe19fcfb53631fcfea88bdd2be162072
civou <civou@civou-PC>
2012-12-21 21:11:41 +0300

    ok

---
+++ malah/src/malah/khugmg.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package malah;
+
+/**
+ *
+ * @author civou
+ */
+public class khugmg {
+    
+    public static void main(String[] args) {
+        System.out.println("dfg");
+    }
+}
